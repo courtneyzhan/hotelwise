b0d5caa9da38c29eb8bc4a2a6fefab1ba17a8bc5
Zhimin Zhan <zhimin@itest2.com>
2016-12-06 19:40:32 +1000

    updated

--- hotelwise-rails/app/controllers/pages_controller.rb
+++ hotelwise-rails/app/controllers/pages_controller.rb
@@ -4,42 +4,43 @@ class PagesController < ApplicationController
   end
   
   def confirmation
-    $credit_card_number = params[:credit_card_number]
-    $credit_card_expiry = params[:credit_card_expiry]
+    @credit_card_number = params[:credit_card_number]
+    @credit_card_expiry = params[:credit_card_expiry]
   end
   
   def customer_register
-    $first_name = params[:first_name]
-    $last_name = params[:last_name]
-    $dob = params[:dob]
-    $email_address = params[:email_address]
-    $confirm_email_address = params[:confirm_email_address]
-    $address_1 = params[:address_1]
-    $address_2 = params[:address_2]
-    $special_req = params[:special_req]
-    if $email_address != $confirm_email_address
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @dob = params[:dob]
+    @email_address = params[:email_address]
+    @confirm_email_address = params[:confirm_email_address]
+    @address_1 = params[:address_1]
+    @address_2 = params[:address_2]
+    @special_req = params[:special_req]
+    if @email_address != @confirm_email_address
       flash[:notice] = "Post successfully created"
     end
   end
   
   def check
-    $num_of_adults = params[:numofadult]
-    $num_of_children = params[:numofchildren]
-    $arrive = params[:arrive]
-    $depart= params[:depart]
-    $room_type= params[:roomtype]
-    if $room_type == "singleov"
-      $room_type = "Single with Ocean View"
-      $price = 30
+    puts "Received params: #{params.inspect}"
+    @num_of_adults = params[:numofadult]
+    @num_of_children = params[:numofchildren]
+    @arrive = params[:arrive]
+    @depart= params[:depart]
+    @room_type= params[:roomtype]
+    if @room_type == "singleov"
+      @room_type = "Single with Ocean View"
+      @price = 30
       redirect_to '/single-ov'
-    elsif $room_type == "Single"
-      $price = 25
+    elsif @room_type == "single"
+      @price = 25
       redirect_to '/single'
-    elsif $room_type == "Double"
-      $price = 40
+    elsif @room_type == "double"
+      @price = 40
       redirect_to '/double'
-    elsif $room_type == "Family"
-      $price = 50
+    elsif @room_type == "family"
+      @price = 50
       redirect_to '/family'
     end
   end
--- hotelwise-rails/config/routes.rb
+++ hotelwise-rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get '/availability_check' => 'pages#check'
+  post '/availability_check' => 'pages#check'
   get '/single-ov' => 'pages#customer_register'
   get '/single' => 'pages#customer_register'
   get '/double' => 'pages#customer_register'
