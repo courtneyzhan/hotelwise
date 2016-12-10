55f3e5599a5d54d499cdb7f6de58b1d59e927366
Zhimin Zhan <zhimin@itest2.com>
2016-12-10 19:53:31 +0800

    checkin booking with overlapping

--- hotelwise-rails/app/models/booking.rb
+++ hotelwise-rails/app/models/booking.rb
@@ -2,5 +2,14 @@ class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :customer
   
+  # Check if a given interval overlaps this interval    
+   def overlaps?(other)
+     (check_in_date - other.check_out_date) * (other.check_in_date - check_out_date) >= 0
+   end
+
+   # Return a scope for all interval overlapping the given interval, including the given interval itself
+   scope :overlapping, lambda { |interval| 
+    where("id <> ? AND room_id = ? AND (DATEDIFF(check_in_date, ?) * DATEDIFF(?, check_out_date)) >= 0", interval.id, interval.room_id, interval.check_out_date, interval.check_in_date)
+   }
 
 end
